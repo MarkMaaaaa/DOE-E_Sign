0cd41c1cf7152bf87966d9c927a9742e32581074
HooradAboo <hoorad.abootalebi@gmail.com>
2022-08-03 13:05:26 -0400

    Added contour filter by perimeter.

--- contour.py
+++ contour.py
@@ -3,7 +3,8 @@ import cv2 as cv
 
 
 # Read image
-image= cv.imread("data/images/panel.jpg")
+image = cv.imread("data/images/panel.jpg")
+# image = cv.imread('panel_circ.jpg')
 
 # Denoise image
 blur = cv.GaussianBlur(image, (5,5), 0)
@@ -37,30 +38,24 @@ print(len(contours))
 
 # Filter contours 
 for i, c in enumerate(contours):
-    # Filter contours by area
-    areaContour=cv.contourArea(c)
-    if areaContour < 350 or areaContour > 550:
+     # Filter by Counter Area
+    areaContour=cv.contourArea(c)   # Calculate area of contour
+    if areaContour < 225 or areaContour > 625:
         contours.pop(i)
+        continue
 
+    # Filter by Counter Perimeter
+    perimeter = cv.arcLength(c, True)
+    if perimeter < 60 or perimeter > 100:
+        contours.pop(i)
+        continue
+
+# print(type(c))
 cv.drawContours(image, contours, contourIdx=-1, color=(125, 125, 0), thickness=2)
 print(len(contours))
-
-# # Find bounding box and extract ROI
-# for c in contours:
-#     x,y,w,h = cv.boundingRect(c)
-#     ROI = image[y:y+h, x:x+w]
-
-# Remove wrong contours
-# out = np.zeros_like(image) # Extract out the object and place into output image
-# out[mask == 255] = image[mask == 255]
-
-# (y, x) = np.where(mask == 255)
-# (topy, topx) = (np.min(y), np.min(x))
-# (bottomy, bottomx) = (np.max(y), np.max(x))
-# out = out[topy:bottomy+1, topx:bottomx+1]
 
 
 # Show result
 cv.imshow("preprocessed", image)
-# cv.imwrite("output.jpg", image)
+cv.imwrite("filter_by_area.jpg", image)
 cv.waitKey(0)
